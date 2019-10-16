f42292d2e0420c90becb53f3cc7a87c8d6be00ad
ivrunner <richyentam@gmail.com>
2019-10-16 11:25:17 -0400

    working with .svg images

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import SEO from "../components/seo";
 import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
 import ttLogoblack from "../images/tt-logo-black.svg";
 import ttLogowhite from "../images/tt-logo-white.svg";
+import ttBlacktext from "../images/Group.svg";
 
 function IndexPage({ data }) {
   return (
@@ -15,13 +16,13 @@ function IndexPage({ data }) {
       />
       <div class='flex flex-wrap items-center justify-center text-center'></div>
       <div className='flex flex-wrap items-center justify-center text-center'>
-        <div className='bg-white text-center sm:w-full relative pin'>
+        <div className='bg-white text-center w-full relative pin'>
           <img
             src={ttLogoblack}
             style={{
               position: "static"
             }}
-            className='block mx-auto w-full py-5'
+            className='block mx-auto w-full py-5 px-5'
             alt='Tap Tourist Logo'
           />
           <h1 className='text-2xl px-1 pb-5'>YOUR MOBILE BEVERAGE SOLUTION</h1>
@@ -29,17 +30,24 @@ function IndexPage({ data }) {
         </div>
       </div>
       <div class='text-center py-5'>
-        <h1 className='text-2xl'>WHAT IS</h1>
-        <h1 className='text-3xl'>TAP TOURIST?</h1>
+        <h1 className='text-pink text-2xl'>WHAT IS</h1>
+        <div className='bg-white text-center w-full relative pin'>
+          <img
+            src={ttBlacktext}
+            style={{
+              position: "static"
+            }}
+            className='block mx-auto w-full px-10'
+            alt='Tap Tourist Logo'
+          />
+        </div>
       </div>
       <div class='text-2xl font-bold inline-block my-0 p-0'>
-        <a href='https://www.target.com/gift-registry/giftgiver?registryId=4c7068963df6485085c9dffff837aa66&type=WEDDING'>
-          <Img
-            fluid={data.imgOne.childImageSharp.fluid}
-            class=''
-            alt='Target Logo'
-          />
-        </a>
+        <Img
+          fluid={data.imgOne.childImageSharp.fluid}
+          class=''
+          alt='Target Logo'
+        />
       </div>
       <p className='text-center py-5 px-2'>
         This is a bunch of dummy text. Neil, this is where you need to think
